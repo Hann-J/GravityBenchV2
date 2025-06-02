c0f8ee54a8e9400bce049820b9b4e89b59c34f3a
Hann-J <jaredlim335@gmail.com>
2025-06-02 17:28:29 -0400

    Period scenario updated, period is invariant under projection

--- scenarios/period.py
+++ scenarios/period.py
@@ -3,8 +3,9 @@ import pandas as pd
 import scripts.task_utils as task_utils
 
 class Scenario:
-    def __init__(self, scenario_creator, skip_simulation=False):
+    def __init__(self, scenario_creator, face_on_projection=False, skip_simulation=False):
         self.scenario_creator = scenario_creator
+        self.face_on_projection = face_on_projection
 
         prompt = """Determine the orbital period of the system."""
         final_answer_units = "s"
@@ -22,7 +23,16 @@ class Scenario:
             indices = np.linspace(0, len(df) - 1, N_obs)
             df = df.iloc[indices].reset_index(drop=True)
 
-        # Calculate period using task_utils
-        # Verification and return_empirical are done in calculate_period
-        period = task_utils.calculate_period(df, self.binary_sim, verification=verification, return_empirical=return_empirical)
-        return period
+        # If face_on_projection is False, we have the full data to calculate the period
+        if not self.face_on_projection:
+            # Calculate period using task_utils
+            # Verification and return_empirical are done in calculate_period
+            period = task_utils.calculate_period(df, self.binary_sim, verification=verification, return_empirical=return_empirical)
+            return period
+        else:
+            # We can get a period from the projection similarly with task_utils but with z=0, period is invariant under projection
+            df['star1_x'] = 0
+            df['star2_x'] = 0
+            period = task_utils.calculate_period(df, self.binary_sim, verification=verification, return_empirical=return_empirical)
+            return period
+            
